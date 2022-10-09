c881b007eb5a7598f7fd546fb28b9356ae209f7e
1064#MarcGI <m.bohner99@gmail.com>
2022-10-09 17:18:34 +0200

    Chapter05

---
+++ Chapter05/exercise08.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	int rc = fork();
+	if (rc < 0)
+	{
+		// fork failed
+		fprintf(stderr, "fork failed\n");
+		exit(1);
+	} else if (rc == 0) {
+		// child (new process)
+		int w = wait(NULL);
+	 	printf("Child wait(): %d\n", w);
+    } else { 
+		// parent goes down this path (main)
+		int w = wait(NULL);
+	 	printf("Parent wait(): %d\n", w);
+    }
+    return 0;
+}
+
+// wait() gibt die processID des childs zurück (wen nFehler dann -1)
+// wird wait() im child aufgerufen, wird -1 (Fehler) zurückgegeben
