d69609f5ff9f7858d003824a0b4a1b2e93b2ed55
1064#MarcGI <m.bohner99@gmail.com>
2022-10-10 14:01:44 +0200

    Chapter05

--- Chapter05/exercise08.c
+++ Chapter05/exercise08.c
@@ -5,27 +5,54 @@
 int main(int argc, char *argv[])
 {
 	int pipefd[2];
+	pid_t cpid;
+	char buf;
 
-	if (pipe(pipefd) == -1)
-	{
-		perror("pipe");
-		exit(EXIT_FAILURE);
-	}
-
-	int rc = fork();
-	if (rc < 0)
+	cpid = fork();
+	if (cpid < 0)
 	{
 		// fork failed
 		fprintf(stderr, "fork failed\n");
 		exit(1);
-	} else if (rc == 0) {
+	} else if (cpid == 0) {
 		// child (new process)
-		int w = wait(NULL);
-	 	printf("Child wait(): %d\n", w);
+		if (pipe(pipefd) == -1)
+		{
+			perror("pipe");
+			exit(EXIT_FAILURE);
+		}
+
+		int rc = fork();
+		if (rc < 0) 
+		{
+			// fork failed
+			fprintf(stderr, "fork failed\n");
+			exit(1);
+		} else if (rc == 0) {
+			// second child
+			//printf("Child 2\n");
+
+			close(pipefd[1]);          /* Close unused write end */
+
+			while (read(pipefd[0], &buf, 1) > 0)
+            	write(STDOUT_FILENO, &buf, 1);
+
+        	write(STDOUT_FILENO, "\n", 1);
+            close(pipefd[0]);
+			_exit(EXIT_SUCCESS);
+		} else {
+			// first child
+			//printf("Child 1\n");
+
+			close(pipefd[0]);          /* Close unused read end */
+            write(pipefd[1], argv[1], strlen(argv[1]));
+            close(pipefd[1]);          /* Reader will see EOF */
+            wait(NULL);                /* Wait for child */
+            exit(EXIT_SUCCESS);
+		}
     } else { 
 		// parent goes down this path (main)
-		int w = wait(NULL);
-	 	printf("Parent wait(): %d\n", w);
+	 	//printf("Parent\n");
     }
     return 0;
 }
