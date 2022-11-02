b9634ff4895c29e2d3db3b82bcfe5dfc58854656
1064#MarcGI <m.bohner99@gmail.com>
2022-11-02 15:37:16 +0100

    aufgabe 5

--- Chapter14/aufgabe5.c
+++ Chapter14/aufgabe5.c
@@ -5,6 +5,10 @@ int main(int argc, char const *argv[])
 {
 	int dataSize = 100;
 	int* data = (int*) malloc(sizeof(int) * dataSize);
+	if (data == NULL){
+		printf("Alloc fehlgeschlagen\n");
+		exit(1);
+	}
 	data[100] = 0;
 	return 0;
 }
