d25b8708910a8b2387cb237a9bc026800591e0d8
1064#MarcGI <m.bohner99@gmail.com>
2022-11-03 15:42:17 +0100

    aufgabe5

--- Chapter15/aufgabe5.py
+++ Chapter15/aufgabe5.py
@@ -6,6 +6,24 @@ import numpy as np
 
 addsize = 1024
 physize = 16 * 1024
+list = [0] * physize
 
-for i in range(addsize):
-	
+for i in range(300):
+    random.seed(i)
+    for j in range(physize):
+        limit = j
+        viraddress = int(addsize * (random.random()))
+
+        if viraddress < limit:
+            list[j] += 1
+
+
+fig = plt.figure()
+x = np.linspace(1, physize, physize)
+plt.plot(x, [u / 20 for u in list], color='orange')
+plt.ylim(0, 1)
+plt.margins(0)
+plt.xlabel('Limit')
+plt.ylabel('Valid fraction (Average)')
+plt.savefig('aufgabe5.png', dpi=227)
+plt.show()
