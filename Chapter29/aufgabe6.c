ce662b584fca55d8d738ac3d6e3185669f8da43d
Marc Bohner <m.bohner99@gmail.com>
2022-12-22 15:01:17 +0000

    better aufgabe5 und aufgabe6

--- Chapter29/aufgabe6.c
+++ Chapter29/aufgabe6.c
@@ -41,12 +41,9 @@ hash_t H;
 
 void* worker(void* arg)
 {
-    // printf("\nThreadID: %d\n\n", (int) arg);
-    for (size_t i = 0; i < 1000000; i++) // wird hier auf 1000001 erhöht, kommt anstatt 4000004 trotzdem 4000000 raus
+    for (size_t i = 0; i < (long int) arg; i++)
     {
-        //Pthread_mutex_lock(&L.lock);
         Hash_Insert(&H, i);
-        //Pthread_mutex_unlock(&L.lock);
     }
     
     return NULL;
@@ -57,10 +54,10 @@ int main(int argc, char const *argv[])
     Hash_Init(&H);
     pthread_t p1, p2, p3, p4, p5;
     long int start = gettime();
-    Pthread_create(&p1, NULL, worker, NULL);
-    Pthread_create(&p2, NULL, worker, NULL);
-    Pthread_create(&p3, NULL, worker, NULL);
-    Pthread_create(&p4, NULL, worker, NULL);
+    Pthread_create(&p1, NULL, worker, (void *) 250000);
+    Pthread_create(&p2, NULL, worker, (void *) 250000);
+    Pthread_create(&p3, NULL, worker, (void *) 250000);
+    Pthread_create(&p4, NULL, worker, (void *) 250000);
     Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
     Pthread_join(p3, NULL);
@@ -72,7 +69,7 @@ int main(int argc, char const *argv[])
 
     Hash_Init(&H);
     start = gettime();
-    Pthread_create(&p5, NULL, worker, NULL);
+    Pthread_create(&p5, NULL, worker, (void *) 1000000);
     Pthread_join(p5, NULL);
     end = gettime();
     time_needed = end - start;
